405b656399572272cab9a5794a9bea63d011261a
ainmx <xxainmxx@protonmail.com>
2023-04-05 01:58:40 +0200

    Add fish

---
+++ .config/fish/functions/dotfiles.fish
@@ -0,0 +1,4 @@
+function dotfiles --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME/' --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/' --description 'alias dotfiles=/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/'
+  /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME/ $argv
+        
+end
