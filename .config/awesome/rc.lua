3068f312655dbd24aedf808d96ecb133feb5efc4
ainmx <xxainmxx@protonmail.com>
2023-06-30 01:04:28 +0200

    Add polybar spawner

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -587,5 +587,4 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
--- beautiful.useless_gap = 5
--- beautiful.gap_single_client = true
+awful.spawn.with_shell("polybar")
